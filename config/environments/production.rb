680345f5032e2f393a8dc4db2ac152392bb5ae9b
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-03-04 11:23:59 -0800

    testing mail on staging site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Vendiwebapp::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'vendistaging.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 25,
+    :user_name            => 'vendistaging@gmail.com',
+    :password             => 'FiBy$ATWrs8LsTG36aPDQ',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
