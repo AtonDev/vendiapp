d0b0a95339fa5084706ddbb91d9b3a374a919302
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-03-12 10:23:26 -0700

    finish first draft of dashboard controller, need to debug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,22 @@
-
+require 'will_paginate/array'
 class DashboardController < ApplicationController
-  def show
-  end
+	def show
+		ordered_notifs = current_seller.notifications.order('created_at DESC')
+		@notifications = ordered_notifs.paginate(page: page_params[:notifications]).per_page(5)
+
+		ordered_proposals = current_seller.price_proposals.order('created_at DESC')
+		ordered_pending_items = ordered_proposals.map{ |p| p.item }
+		@pending_items = ordered_pending_items.paginate(page: page_params[:pending]).per_page(5)
+
+		ordered_selling_items = current_seller.items.order('created_at ASC')
+		@selling_items = ordered_selling_items.paginate(page: page_params[:selling]).per_page(5)
+
+		@ledger_items = Item.limit(5).order("RANDOM()")
+	end
+
+	private 
+		def page_params
+			params.require(:page).permit(:notifications, :pending, :selling)
+		end
+
 end
--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -51,20 +51,6 @@ class SellersController < ApplicationController
 	end
 
 
-	#eventually this method needs to go to the owner controller
-	def commit_item
-		item = Item.find item_id
-		item.sale_info.proposed_price = price
-		item.save
-		current_seller.items << item
-		if current_seller.save
-			flash.now[:success] = "You committed to sell kart 100 for #{price} USD."
-			render 'show'
-		else
-			flash[:warning] = "something went wrong"
-			item_index
-		end
-	end
 
 	def item_index
 		@items = Item.all
