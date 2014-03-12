3a33705a75a896e0508476182849b53fa616ac27
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-03-12 11:58:30 -0700

    add pagination

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,22 +1,18 @@
 require 'will_paginate/array'
 class DashboardController < ApplicationController
 	def show
+
 		ordered_notifs = current_seller.notifications.order('created_at DESC')
-		@notifications = ordered_notifs.paginate(page: page_params[:notifications]).per_page(5)
+		@notifications = ordered_notifs.paginate(page: params[:notifications_page]).per_page(5)
 
 		ordered_proposals = current_seller.price_proposals.order('created_at DESC')
 		ordered_pending_items = ordered_proposals.map{ |p| p.item }
-		@pending_items = ordered_pending_items.paginate(page: page_params[:pending]).per_page(5)
+		@pending_items = Item.all().paginate(page: params[:pending_page]).per_page(5)
 
 		ordered_selling_items = current_seller.items.order('created_at ASC')
-		@selling_items = ordered_selling_items.paginate(page: page_params[:selling]).per_page(5)
+		@selling_items = ordered_selling_items.paginate(page: params[:selling_page]).per_page(5)
 
 		@ledger_items = Item.limit(5).order("RANDOM()")
 	end
-
-	private 
-		def page_params
-			params.require(:page).permit(:notifications, :pending, :selling)
-		end
 
 end
