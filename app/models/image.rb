0a4bfafcd67a5aca1681883dc849c2afa2ae95b9
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-03-12 21:19:05 -0700

    fix paperclip not working in production attempt 4

--- app/models/image.rb
+++ app/models/image.rb
@@ -19,7 +19,12 @@ class Image < ActiveRecord::Base
 		square: '200x200#',
 		medium: '300x300>'
 	},
-	:storage => "s3"
+	:storage => "s3", 
+	:bucket => ENV['S3_BUCKET_NAME'],
+        :s3_credentials => {
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+       }
 
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
