f817c68ace1b7c25c67a7fbf894bf03d0d46d5bb
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-03-12 21:36:59 -0700

    fix paperclip finally solved

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
-if Rails.env == "staging" 
+if Rails.env == "staging" or Rails.env == "development"
    S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_KEY'], :bucket => "vendistorage"} 
  else 
-   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
+   S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_KEY'], :bucket => "vendiapp"} 
 end
