3b5a6ca2287cc8393e8bf41c5e223b190ccda840
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-03-18 11:43:08 -0700

    add available condition

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+
+  def condition_select_options
+    r = []
+    Item.available_conditions.each_with_index do |c, idx|
+      r << [c, idx]
+    end
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -19,19 +19,28 @@ class Item < ActiveRecord::Base
 	has_many :images, dependent: :destroy
 	has_one :sale_info, dependent: :destroy
 	has_many :price_proposals, dependent: :destroy
-
 	after_save :init_sale_info
-
 	accepts_nested_attributes_for :images, allow_destroy: true
-	#def add_user(user_id)
-	#	unless user_id.nil?
-	#		user = User.find(user_id)
-	#		unless self.users.include? user
-	#			self.users << user
-	#			self.save
-	#		end
-	#	end
-	#end
+
+	def self.available_conditions 
+		['New', 'Used - Like New', 'Used - Good', 'Used - Acceptable']
+	end
+
+	 def condition_conversion(cond)
+    if cond.is_a? Integer and cond < available_conditions.length
+      available_conditions[cond]
+    elsif cond.is_a? String and available_conditions.include?(cond)
+      available_conditions.index(cond)
+    end
+  end
+
+	def condition=(condition)
+		if cond.is_a? String and available_conditions.include?(cond)
+      write_attribute(:condition,  available_conditions.index(cond))
+    end
+  end
+
+
 
 	private
 		def init_sale_info
