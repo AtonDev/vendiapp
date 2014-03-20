50439e7ef26614dc4bb14772b4659032becdb7a6
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-03-19 18:19:37 -0700

    complete basic twilio implmentation

--- app/controllers/sellers/registrations_controller.rb
+++ app/controllers/sellers/registrations_controller.rb
@@ -4,22 +4,11 @@ def new_phone_verification
   build_resource sign_up_params
 
   if resource.valid?(:create)
-    @phone_verification_code = "%04d" % rand(9999)
-
+    v_code = create_verification_code
+    @code_id = v_code.id
+    @phone_number = sign_up_params[:phone_number]
     ## send text message
-    number_to_send_to = sign_up_params[:phone_number]
-    twilio_sid = "AC31969b5308cfc7d3532bafd6c5f770e9"
-    twilio_token = "92e721db272c2f8e4070fd8ffc0ca536"
-    twilio_phone_number = "5103808583"
- 
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
- 
-    @twilio_client.account.sms.messages.create(
-      :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
-      :body => "Hola Mercedes, #{@phone_verification_code}"
-    )
-
+    send_verification_sms(@phone_number, v_code.code)
     ## render form to enter verification code
     render template: "phone/verification"
   else
@@ -32,15 +21,13 @@ end
 # send create form to this method 
 def verify_phone
   debugger
-  if phone_code_param[:code] == @phone_verification_code
-    ## update phone verified?
+  verification_code_obj = VerificationCode.find(phone_code_param[:code_id])
+  if phone_code_param[:code] == verification_code_obj.code
+    verification_code_obj.destroy
     render 'create'
   else
     flash[:danger] = "Your verification code did not match try again."
     redirect_to :back
-    #@sign_up_params = nil
-    #build_resource @sign_up_params
-    #render 'new'
   end
 end
 
@@ -55,6 +42,27 @@ protected
   end
 
   def phone_code_param
-    params.permit(:code)
+    params.permit(:code, :code_id)
   end
+
+  def create_verification_code
+    code_str = "%04d" % rand(9999)
+    VerificationCode.create(:code => code_str)
+  end
+
+  def send_verification_sms(number_to_send_to, verification_code)
+    number_to_send_to = sign_up_params[:phone_number]
+    twilio_sid = "AC31969b5308cfc7d3532bafd6c5f770e9"
+    twilio_token = "92e721db272c2f8e4070fd8ffc0ca536"
+    twilio_phone_number = "5103808583"
+ 
+    twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+ 
+    twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => "Hola Mercedes, #{verification_code}"
+    )
+  end
+
 end
