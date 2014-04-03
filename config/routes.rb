570c5afac5380ef7fc1b61e4038e694c86b4ed78
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-04-03 12:50:05 -0700

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Vendiwebapp::Application.routes.draw do
 
 	## Mobile API
 	namespace :api do
-		post 'add_image' => 'phonegap_items#add_image', :as => :add_image
+		get 'add_image' => 'phonegap_items#add_image', :as => :add_image
 		post 'add_info' => 'phonegap_items#add_info', :as => :add_info
 	end
 
