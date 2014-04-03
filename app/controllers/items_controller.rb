10686e61305ae8eb6d0cb3f305dbde3a38d917ae
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-04-03 06:45:40 -0700

    implement ability to look at subset of items when you are not signed in.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
       items = Item.where(:available => true)
       @items = items.paginate(page: params[:page], per_page: 12)
     else
-      redirect_to :new_seller_session
+      @items = Item.all(:conditions => ["available >= ?", true], :order => "RANDOM()", :limit => 8)
     end
   end
 
