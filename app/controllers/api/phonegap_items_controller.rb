568323a52adc353c31c3c777b44a7435674dcbbc
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-04-05 06:35:13 -0700

    comment out methods implementations

--- app/controllers/api/phonegap_items_controller.rb
+++ app/controllers/api/phonegap_items_controller.rb
@@ -7,21 +7,21 @@ class Api::PhonegapItemsController < ApplicationController
   def add_image
     # tmpFilePath = params[:file][:tempfile].path
     #Mailer.send_pgimage(tmpFilePath)
-    render :json => params
-    return
-    img = Image.new()
-    img.photo = params[:picture]
-    if img.save
-      render :json => {:status => "success"}
-      return
-    else
-      render :json => {:status => "fail"}
-    end
+    #render :json => params
+    #return
+    #img = Image.new()
+    #img.photo = params[:picture]
+    #if img.save
+    #  render :json => {:status => "success"}
+    #  return
+    #else
+    #  render :json => {:status => "fail"}
+    #end
   end
 
   def add_info
-    Mailer.send_pginfo(params[:info])
-    render :json => {:status => "success"}
+    #Mailer.send_pginfo(params[:info])
+    #render :json => {:status => "success"}
   end
 
   def pong
